411ee2028a8d5164558779e0db3b20b87dba80b7
sameer-shaikh <sameermubeen4@gmail.com>
2021-07-23 19:47:54 +0500

    bug fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,35 +7,37 @@ import logo from "../assets/logo.png";
 const Footer = () => {
 	return (
 		<footer className="footer">
-			<div className="container">
-				<div className="row">
-					<div className="col-12 col-sm-6 mb-4 mb-sm-0">
-						<div className="mb-2">
-							<NavLink to="/">
-								<img src={logo} alt="" className="logo" />
-							</NavLink>
+			<div className="page_container">
+				<div className="container-fluid">
+					<div className="row">
+						<div className="col-12 col-sm-6 mb-4 mb-sm-0">
+							<div className="mb-2">
+								<NavLink to="/">
+									<img src={logo} alt="" className="logo" />
+								</NavLink>
+							</div>
+
+							<div>Copyright © 2021 minilink test</div>
 						</div>
 
-						<div>Copyright © 2021 minilink test</div>
-					</div>
-
-					<div className="col-12 col-sm-6">
-						<a
-							className="text-decoration-none text-dark"
-							href="https://google.com"
-							target="_blank"
-						>
-							Google
-						</a>
-						<br />
-						<a
-							className="text-decoration-none text-dark"
-							href="https://minilink.bio/"
-							target="_blank"
-						>
-							Contacto
-						</a>
-						<br />
+						<div className="col-12 col-sm-6">
+							<a
+								className="text-decoration-none text-dark"
+								href="https://google.com"
+								target="_blank"
+							>
+								Google
+							</a>
+							<br />
+							<a
+								className="text-decoration-none text-dark"
+								href="https://minilink.bio/"
+								target="_blank"
+							>
+								Contacto
+							</a>
+							<br />
+						</div>
 					</div>
 				</div>
 			</div>
